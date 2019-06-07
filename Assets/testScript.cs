caadc4f6b7c075a7052f9434649aa9d6efbda7c9
Joshua <joshua@orangemason.com>
2019-06-07 11:42:59 +0200

    first commit

---
+++ Assets/testScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MLHands.Start();
+        MLInput.Start();
+        Debug.Log("Test successful");
+    }
+
+    private void OnDestroy()
+    {
+        MLHands.Stop();
+        MLInput.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
